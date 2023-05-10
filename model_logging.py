f1baf5356d3b6b1c0759a5ff646e2dd0596221ba
stan-liao <stan.liao@gmail.com>
2023-05-10 04:25:31 -0400

    remove scalar log

--- model_logging.py
+++ model_logging.py
@@ -97,12 +97,14 @@ class TensorboardLogger(Logger):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
+        '''
+
         with self.writer.as_default():
             tf.summary.scalar(tag, value, step=step)
-        '''
         summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
         self.writer.add_summary(summary, step)
         '''
+        return
 
     def image_summary(self, tag, images, step):
         """Log a list of images."""
