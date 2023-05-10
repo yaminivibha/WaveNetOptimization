a8df4a61bab980c49e3cf3a9d2f619808bdbd5ac
stan-liao <stan.liao@gmail.com>
2023-05-10 05:28:43 -0400

    bleh

--- model_logging.py
+++ model_logging.py
@@ -102,7 +102,7 @@ class TensorboardLogger(Logger):
         my_value = value
 
         with self.writer.as_default():
-            tf.summary.scalar(tag, my_value, step=step)
+            tf.summary.scalar(tag, torch.Tensor.cpu(my_value).item(), step=step)
 
         '''
         summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
