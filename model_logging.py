84717491677079f28663c050802880e631baf0e0
stan-liao <stan.liao@gmail.com>
2023-05-10 04:52:49 -0400

    fixes

--- model_logging.py
+++ model_logging.py
@@ -101,7 +101,7 @@ class TensorboardLogger(Logger):
         
 
         with self.writer.as_default():
-            tf.summary.scalar(tag, torch.Tensor.cpu(value), step=step)
+            tf.summary.scalar(tag, int(torch.Tensor.cpu(value)), step=step)
 
         '''
         summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
